f0b47356bea6f24b6dd048bc1eece28c0b8d4baf
mariannegit <mtrubets@itcollege.ee>
2016-10-08 05:51:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Minu projekt");
+        System.out.println("Word Wizard Worker");
         System.out.println("Autor: mariannegit");
         System.out.println("Aasta: 2016");
     }
